f26d76334a73620d17d4c141a9642c0117a0a294
Maria Clara <146785278+MARIACLARA6041@users.noreply.github.com>
2023-11-21 20:14:28 -0300

    Update fibonacci.test.js

--- entregavel 4 alzir/fibonacci.test.js
+++ entregavel 4 alzir/fibonacci.test.js
@@ -1,24 +1,9 @@
-function fibonacci(quanridade_numeros) {
-    let n1, n2, sucessor;
-    n1=0;
-    n2=1;
+const fibonnaci = require('../funcoes/fibonacci');
 
-    if (quantidade_numeros <=0) {
-        return[]
-    }else if (quantidade_numeros ==1) {
-        return [0]
-    }else if (quanridade_numeros ==2) {
-        return [0,1]
-    }
+test('os 10 primeiros números da contagem de fibonacci devem ser os corretos', () => {
+    expect(fibonnaci(10)).toMatchObject([0, 1, 1, 2, 3, 5, 8, 13, 21, 34]);
+});
+test('os 2 primeiros números da contagem de fibonacci devem ser 0 e 1', () => {
+    expect(fibonnaci(2)).toMatchObject([0, 1]);
+});
 
-    numeros = [0,1]
-    for (i =2; i< quantidade_numeros; i ++){
-        sucessor = n1 + n2;
-        n1 = n2;
-        n2 = sucessor;
-        numero.push(sucessor);
-    }
-    return numero
-
-}
-module.exports = fibonacci;
