c54d7b5f910fae01fd01aa07c2edcd8da4a56e01
Maria Clara <146785278+MARIACLARA6041@users.noreply.github.com>
2023-11-23 16:33:30 -0300

    Update fibonacci.js

--- entregavel 4 alzir/fibonacci.js
+++ entregavel 4 alzir/fibonacci.js
@@ -1,24 +1,25 @@
-function fibonacci(quanridade_numeros) {
+function fibonacci(quantidade_numeros) {
     let n1, n2, sucessor;
-    n1=0;
-    n2=1;
+    n1 = 0;
+    n2 = 1;
 
-    if (quantidade_numeros <=0) {
-        return[]
-    }else if (quantidade_numeros ==1) {
-        return [0]
-    }else if (quanridade_numeros ==2) {
-        return [0,1]
+    if (quantidade_numeros <= 0) {
+        return [];
+    } else if (quantidade_numeros == 1) {
+        return [0];
+    } else if (quantidade_numeros == 2) {
+        return [0, 1];
     }
 
-    numeros = [0,1]
-    for (i =2; i< quantidade_numeros; i ++){
+    let numeros = [0, 1];
+    for (let i = 2; i < quantidade_numeros; i++) {
         sucessor = n1 + n2;
         n1 = n2;
         n2 = sucessor;
-        numero.push(sucessor);
+        numeros.push(sucessor);
     }
-    return numero
-
+    return numeros;
 }
+
 module.exports = fibonacci;
+
