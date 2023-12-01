4f23458a3059a206be82b8722190f589a5c50c92
Maria Clara <146785278+MARIACLARA6041@users.noreply.github.com>
2023-12-01 14:14:17 -0300

    fibonacci.java

--- entregavel 1 alzir/function mdc(a, b) {.js
+++ entregavel 1 alzir/function mdc(a, b) {.js
@@ -1,11 +1,36 @@
-function mdc(a, b) {
-    while (b !== 0) {
-        const temp = b;
-        b = a % b;
-        a = temp;
-    }
-    return Math.abs(a);
-}
+import java.util.ArrayList;
+import java.util.List;
 
-// Exemplo de uso:
-console.log(mdc(48, 18)); // Saída: 6
+class Main {
+  public static void main(String[] args) {
+    int N = 10; // Substitua 10 pelo número de termos que você deseja gerar
+        List<Integer> resultado = fibonacci(N);
+
+        System.out.println(resultado);
+  }
+
+  public static List<Integer> fibonacci(int N) {
+        List<Integer> sequence = new ArrayList<>();
+
+        if (N <= 0) {
+            return sequence;
+        } else if (N == 1) {
+            sequence.add(0);
+            return sequence;
+        } else if (N == 2) {
+            sequence.add(0);
+            sequence.add(1);
+            return sequence;
+        }
+
+        sequence.add(0);
+        sequence.add(1);
+
+        for (int i = 2; i < N; i++) {
+            int nextTerm = sequence.get(i - 1) + sequence.get(i - 2);
+            sequence.add(nextTerm);
+        }
+
+        return sequence;
+    }
+}
