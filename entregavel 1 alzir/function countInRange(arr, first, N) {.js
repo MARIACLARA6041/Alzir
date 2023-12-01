2ae55bb0dfbf7a1b654b84e617d4d1779d723de7
Maria Clara <146785278+MARIACLARA6041@users.noreply.github.com>
2023-12-01 14:26:31 -0300

    delete

--- entregavel 1 alzir/function countInRange(arr, first, N) {.js
+++
@@ -1,6 +0,0 @@
-function countInRange(arr, first, N) {
-    return arr.filter(num => num >= first && num <= N).length;
-}
-
-// Exemplo de uso:
-console.log(countInRange([1, 2, 3, 4, 5, 6, 7, 8, 9], 3, 7)); // Saída: 5
